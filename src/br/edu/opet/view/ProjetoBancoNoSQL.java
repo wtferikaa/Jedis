b47612469b7c0ecce122323116dec0ebbc9dbdf5
Erika <erikapaiva7@gmail.com>
2017-11-10 00:59:29 -0200

    comentários

--- src/br/edu/opet/view/ProjetoBancoNoSQL.java
+++ src/br/edu/opet/view/ProjetoBancoNoSQL.java
@@ -10,6 +10,7 @@ import redis.clients.jedis.Jedis;
 
 public class ProjetoBancoNoSQL {
 
+	//definindo os formatadores de data e hora
 	private static DateTimeFormatter sFormatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	private static DateTimeFormatter sFormatadorDataeHora = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm:ss");
 
@@ -19,7 +20,8 @@ public class ProjetoBancoNoSQL {
 		System.out.println("Connecção com o servidor");
 		// Testando se o servidor está execuando
 		System.out.println("Servidor está executando: " + jedis.ping());
-
+        
+		//definindo a variavel jedis
 		Jedis tJedis = new Jedis();
 		menu1: while (true) {
 			System.out.println("MENU");
@@ -34,25 +36,25 @@ public class ProjetoBancoNoSQL {
 
 			opcao: switch (tOpcao) {
 			case 1: // Criando o novo usuario
-				LocalDate tDataCadastro = LocalDate.now();
+				LocalDate tDataCadastro = LocalDate.now(); //data do cadastro é a data de agora, presente
 				String tApelido = Leitor.readString("Apelido:");
 				String tNome = Leitor.readString("Nome:");
-				if (tNome == "") {
+				if (tNome == "") {//nome não pode ser vazio
 					break opcao;
 				}
 
 				while (true) {
 
-					if (tApelido == "") {
+					if (tApelido == "") {//apelido não pode ser vazio 
 						break opcao;
 					}
-
+                    //definindo a variavel tApelidoBd1 que irá ter a função de pegar o apelido que está gravado no banco de dados
 					String tApelidoBd1 = tJedis.hget("Usuário:" + tApelido, "Apelido");
-
+                    //se já estiver gravado no banco, retorna mensagem de erro 
 					if (!(tApelidoBd1 == null)) {
 						System.out.println("Este apelido já existe, digite novamente");
 					}
-
+                    //gravando os dados que o usuário irá inserir no banco 
 					tJedis.hset("Usuario:" + tApelido, "Nome", tNome);
 					tJedis.hset("Usuario:" + tApelido, "Apelido", tApelido);
 					tJedis.hset("Usuario:" + tApelido, "DataCadastro", tDataCadastro.format(sFormatador));
@@ -82,7 +84,7 @@ public class ProjetoBancoNoSQL {
 					}
 
 					else {
-						menu2: while (true) {
+						menu2: while (true) {//menu depois do login
 
 							System.out.println();
 							System.out.println("1 - Enviar Mensagem");
@@ -101,12 +103,13 @@ public class ProjetoBancoNoSQL {
 								LocalDateTime tDataMensagem = LocalDateTime.now();
 
 								System.out.println();
+								//definindo a variavel para o usuario digitar para quem irá ser enviada a mensagem
 								String tDestinatario = Leitor.readString("Para:");
 								// Destinatario não pode ser vazia
-								if (tDestinatario == "") {
+								if (tDestinatario == "") {//não pode ser vazio
 									break opcao2;
 								}
-
+                                //definindo a variavel para o usuario inserir a mensagem
 								String tMensagem = Leitor.readString("Mensagem:");
 
 								// a mensagem não pode ser vazia
@@ -114,10 +117,10 @@ public class ProjetoBancoNoSQL {
 									break opcao2;
 								}
 
-								// montando a chave e valor
+								// montando a chave e valor, guardando as informações junto com a data e hora
 								tJedis.sadd(
 										tApelidoInformado + ":" + tDataMensagem.format(sFormatadorDataeHora) + ":Para",
-										tDestinatario);
+										tDestinatario);//apelido unico
 
 								tJedis.set(tApelidoInformado + ":" + tDataMensagem.format(sFormatadorDataeHora) + ":De",
 										tApelidoInformado);
@@ -125,18 +128,20 @@ public class ProjetoBancoNoSQL {
 								tJedis.set(tApelidoInformado + ":" + tDataMensagem.format(sFormatadorDataeHora)
 										+ ":Mensagem", tMensagem);
 
-								// incrementando
+								// incrementando quantas mensagens foram enviadas 
 								Long tSaida = tJedis.incr(tApelidoInformado + "--saida");
-
+                                //um conjunto ordenado do remetente e quantas mensagens foram enviadas 
 								tJedis.zadd(tApelidoInformado + "--saida ", tSaida, tApelidoInformado + ":" + tDataMensagem.format(sFormatadorDataeHora));
 
 								String[] SepararTexto = tDestinatario.split(",");
+								//contador para ir incrementando as mensagens recebidas 
 								for (int i = 0; i < SepararTexto.length; i++) {
 									
 									Long tEntrada = tJedis.incr(SepararTexto[i] + "--entr  ");
 
 									//System.out.println(SepararTexto[i] + "-entrada  " + tEntrada + ":"+ tApelidoInformado + ":" + tDataMensagem.format(sFormatadorDataeHora));
-
+                                    
+									//guardando no banco as mensagens recebidas
 									tJedis.zadd(SepararTexto[i] + "--entr ", tEntrada, tApelidoInformado + ":" + tDataMensagem.format(sFormatadorDataeHora));
 								}
 
@@ -145,14 +150,16 @@ public class ProjetoBancoNoSQL {
 							case 2:
 								System.out.println("Visualizar mensagens recebidas");
 
-								// zlexcount pois o count normal não está funcionando
+								// zlexcount pois o count normal não está funcionando, conta a quantidade de mensagens recebidas
+								//e mostra na tela
 								Long contarEntrada = tJedis.zlexcount(tApelidoInformado + "--entr ", "-", "+");
 								System.out.println("Você tem " + contarEntrada + " mensagens!");
-
+                                // se o usuario for impopular e nao receber mensagem de ninguem, o programa volta pro segundo menu
 								if (contarEntrada == 0) {
 
 									break opcao2;
 								} else {
+									//mostra as mensagens recebidas em uma lista ordenada
 									for (int i = 0; i < contarEntrada; i++) {
 										System.out.println(
 												(1 + i) + " " + tJedis.zrange(tApelidoInformado + "--entr ", i, i));
@@ -161,15 +168,17 @@ public class ProjetoBancoNoSQL {
 
 									// variavel para ver mensagem
 									Long tVisualizar = Leitor.readLong("Visualisar mensagem:");
+									//verificando se a mensagem existe ou não
 									if (tVisualizar == 0) {
 										break opcao2;
 									} else {
+										//mostrando o remetente e quantas mensagens foram enviadas
 										Set<String> tVerMensagem = tJedis.zrange(
 												tApelidoInformado + "--entr ", tVisualizar - 1, tVisualizar - 1);
-
+                                        // tirando o [ e o ] 
 										String cortarString = tVerMensagem.toString();
 										String tVerMensagem2 = cortarString.substring(1, cortarString.length() - 1);
-
+                                        // mostrando na tela quem enviou a mensagem e a mensagem
 										System.out.println(tJedis.get(tVerMensagem2 + ":De") + ": "
 												+ tJedis.get(tVerMensagem2 + ":Mensagem"));
 										System.out.println();
@@ -183,13 +192,16 @@ public class ProjetoBancoNoSQL {
 										} else {
 											LocalDateTime tDataMensagem2 = LocalDateTime.now();
 
+											//mostrando a quantidade de respostas para a mensagem
 											Long tEntradaResposta = tJedis.incr(tVerMensagem2 + "-resp  ");
 
 											tJedis.zadd(tVerMensagem2 + "--resp  ", tEntradaResposta,
 													tApelidoInformado + ":"
 															+ tDataMensagem2.format(sFormatadorDataeHora));
+											//definindo a variavel que vai dar get na mensagem que o enviaram
 											String tDestinatario2 = tJedis.get(tVerMensagem2 + ":De");
 
+											//guardando tudinho no banco
 											tJedis.sadd(
 													tApelidoInformado + ":"
 															+ tDataMensagem2.format(sFormatadorDataeHora) + ":Para",
@@ -203,12 +215,16 @@ public class ProjetoBancoNoSQL {
 															+ tDataMensagem2.format(sFormatadorDataeHora) + ":Mensagem",
 													tResposta);
 
+											//incrementando as saidas de mensagem
 											Long tSaida2 = tJedis.incr(tApelidoInformado + "--saida");
+											//guarando no redis um conjunto ordenado de quantas mensagens foram enviadas
 											tJedis.zadd(tApelidoInformado + "--saida ", tSaida2, tApelidoInformado + ":"
 													+ tDataMensagem2.format(sFormatadorDataeHora));
-
+											
+											//incrementando quem vai receber
 											Long tEntrada2 = tJedis.incr(tDestinatario2 + "-entr ");
 
+											//guardando um conjunto ordenado de novo das mensagens que chegaram para o usuario
 											tJedis.zadd(tDestinatario2 + "--entr ", tEntrada2, tApelidoInformado
 													+ ":" + tDataMensagem2.format(sFormatadorDataeHora));
 
@@ -221,16 +237,18 @@ public class ProjetoBancoNoSQL {
 							case 3:
 								System.out.println("Visualizar mensagens enviadas");
 
+								//contando quantas mensagens foram enviadas e mostrando
 								Long contarSaida = tJedis.zlexcount(tApelidoInformado + "--saida ", "-", "+");
 								System.out.println("Você enviou " + contarSaida + " mensagens!");
 
-								if (contarSaida == 0) {
+								if (contarSaida == 0) {//ai que preguiça de comentar isso
 
 									break opcao2;
 								} else {
 
+									
 									for (int i = 0; i < contarSaida; i++) {
-
+                                        //mostra as mensagens enviadas enquanto a condição for verdadeira
 										System.out.println(
 												(1 + i) + " " + jedis.zrange(tApelidoInformado + "--saida ", i, i));
 									}
@@ -240,35 +258,46 @@ public class ProjetoBancoNoSQL {
 										System.out.println();
 										Long tVerSaida = Leitor.readLong("Visualisar mensagem:");
 
+										// n dá pro usuario digitar 0 ou outro numero menor que zero
+										//na hora de visualizar a mensagem
 										if (tVerSaida == 0 || tVerSaida < 0) {
 											break opcao2;
 										} else {
+											//definindo a variavel que irá pegar da lista do banco as mensagens que o usuario enviou
 											Set<String> tVerMensagem3 = tJedis.zrange(tApelidoInformado + "--saida ",
 													tVerSaida - 1, tVerSaida - 1);
 
+											//transforma em string
 											String Cortar = (tVerMensagem3).toString();
+											// e tira o [ e o ] pra sair da chave
 											String tVerMensagem4 = Cortar.substring(1, Cortar.length() - 1);
 
+											//mostra o destino da mensagem, pra que vai ir
 											System.out.println("Para: " + tJedis.smembers(tVerMensagem4 + ":Para"));
+											//mostra a mensagem enviada
 											System.out.println(tJedis.get(tVerMensagem4 + ":Mensagem"));
 
+											//conta as repostas que o usuario enviou e mostra na tela
 											Long contarResposta = tJedis.zlexcount(tVerMensagem4 + "--resp ", "-",
 													"+");
 											System.out.println();
 											System.out.println("Você possui " + contarResposta + " respostas.");
 
-											if (contarResposta == 0) {
+											if (contarResposta == 0) {// se for zero volta pro menu
 												break opcao3;
 											} else {
 												System.out.println("Respostas:");
 
+												//pegando da lista as respostas enviadas
 												for (int i = 0; i < contarResposta; i++) {
 													Set<String> tVerMensagem5 = tJedis
 															.zrange(tVerMensagem4 + "--resp ", i, i);
-
+													
+													//transformando em string e tirando o [ e ] denovo
 													String Cortar2 = (tVerMensagem5.toString());
 													String tTirarChave = Cortar2.substring(1, Cortar2.length() - 1);
 
+													// pegando do banco quem enviou a reposta e mostrando o nome de quem é
 													String tNome2 = tJedis.get(tTirarChave + ":De");
 													System.out.println((1 + i) + "- " + tNome2);
 												}
@@ -277,16 +306,20 @@ public class ProjetoBancoNoSQL {
 												if (tVerResposta == 0 || tVerResposta < 0) {
 													break opcao2;
 												} else {
+													// pegando da lista
 													Set<String> tVerMensagem6 = tJedis.zrange(
 															tVerMensagem4 + "--resp  ", tVerResposta - 1,
 															tVerResposta - 1);
-
+													
+													//transformando em string e tirando o [ e ] denovo
 													String Cortar3 = (tVerMensagem6).toString();
 													String tTirarChave2 = Cortar3.substring(1, Cortar3.length() - 1);
 
+													// Mostrando a mensagem que foi enviada
 													System.out.println(tJedis.get(tTirarChave2 + ":Mensagem"));
 
-													String opf = Leitor.readString("Enter para Sair!");
+													//pede pra sair
+													String opf = Leitor.readString("Digite enter para sair");
 													if (opf == "")
 														break opcao3;
 												}
@@ -295,7 +328,8 @@ public class ProjetoBancoNoSQL {
 									}
 								}
 								break opcao2;
-
+  
+							//aqui nóis mostra tudo que foi cadastrado
 							case 4:
 								System.out.println("Visualização dos dados");
 								System.out.println();
@@ -307,6 +341,7 @@ public class ProjetoBancoNoSQL {
 
 								break opcao2;
 
+								//sai do login
 							case 5:
 								break opcao;
 
@@ -315,6 +350,7 @@ public class ProjetoBancoNoSQL {
 					}
 				}
 
+				//sai do programa
 			case 3:
 				break menu1;
 			}
